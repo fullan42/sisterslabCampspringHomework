1b2ac4d96bf4a7ef6f72d3be88ef10450c8f6683
fullan42 <hasan_-1@outlook.com>
2024-02-21 15:51:02 +0000

    init

---
+++ src/main/java/com/example/sisterslabspringhomework/SisterslabspringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sisterslabspringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisterslabspringHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisterslabspringHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sisterslabspringhomework/SisterslabspringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sisterslabspringhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisterslabspringHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
